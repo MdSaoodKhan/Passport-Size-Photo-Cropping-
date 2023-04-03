340c0e69299a3b7719e885f475b8c5a2b9e3c724
Saood Khan <mdsaoodkhan007@gmail.com>
2023-04-03 14:00:14 +0530

    merged

--- Cropping_Code.py
+++ Cropping_Code.py
@@ -1,11 +1,19 @@
 import cv2
 import os
 
-Og_dir = 'E:\Project\Passport_Photo\Original'
-Crp_dir = 'E:\Project\Passport_Photo\Cropped\\'
-notDetect_dir = 'E:\Project\Passport_Photo\\NotDetected\\'
+Og_dir = 'E:\Project\Passport_Photo\Passport-Size-Photo-Cropping-Using-OpenCV\Original'
+Crp_dir = 'E:\Project\Passport_Photo\Passport-Size-Photo-Cropping-Using-OpenCV\Cropped\\'
+notDetect_dir = 'E:\Project\Passport_Photo\Passport-Size-Photo-Cropping-Using-OpenCV\\NotDetected\\'
 
-def rotate_image(image):
+for file_name in os.listdir(Crp_dir):
+    file = Crp_dir + file_name
+    os.remove(file)
+
+for file_name in os.listdir(notDetect_dir):
+    file = notDetect_dir + file_name
+    os.remove(file)
+
+def ImageRotation(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=42)
     print(len(faces))
@@ -18,26 +26,17 @@ def rotate_image(image):
                 return faces,image
     return faces,image
 
-for file_name in os.listdir(Crp_dir):
-    file = Crp_dir + file_name
-    os.remove(file)
-
-for file_name in os.listdir(notDetect_dir):
-    file = notDetect_dir + file_name
-    os.remove(file)
-
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 for filename in os.listdir(Og_dir):
     image = cv2.imread(os.path.join(Og_dir, filename))
     print(filename)
-
     if image is None:
-        print(filename," - Error 404: Image not found")
+        print(filename," - Image not found (Corrupt Image)")
         continue
 
-    faces,new_image = rotate_image(image)
+    faces,new_image = ImageRotation(image)
     height,width,useless=new_image.shape
 
     if len(faces) == 0:
@@ -45,24 +44,24 @@ for filename in os.listdir(Og_dir):
         continue
 
     for (x, y, w, h) in faces:
-        u = int(h*0.5)
-        o=u
-        v = int(w*0.45)
-        p=v
+        a = int(h*0.5)
+        o=a
+        b = int(w*0.45)
+        p=b
 
-        if (y-u<0):
-            u = y
-        if (x-v<0):
-            v = x
-        if((y+h+u-10)>height):
+        if (y-a<0):
+            a = y
+        if (x-b<0):
+            b = x
+        if((y+h+a-10)>height):
             o=height
-        if((x+w+v)>width):
+        if((x+w+b)>width):
             p=width
-        if((y+h+o-10)-(y-h)<=200 or ((x+w+p)-(x-v)<=200)):
+        if((y+h+o-10)-(y-h)<=200 or ((x+w+p)-(x-b)<=200)):
             cv2.imwrite(os.path.join(notDetect_dir, filename), image)
             break
 
-        cropped_face = new_image[y-u:y+h+o-10, x-v:x+w+p]
+        cropped_face = new_image[y-a:y+h+o-10, x-b:x+w+p]
         cv2.imwrite(os.path.join(Crp_dir, filename), cropped_face)
         break
 
